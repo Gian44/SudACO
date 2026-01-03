27327f239d8a687a1f9e87ecc66930a073d7b2cb
Gian44 <110678169+Gian44@users.noreply.github.com>
2026-01-04 06:38:28 +0800

    update
    Setup project in thesis laptop

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 @echo off
 echo Building Sudoku Solver...
-"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild.exe" .\vs2017\sudoku_ants.vcxproj /t:Build /p:Configuration=Release /p:Platform=x64
+"C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\MSBuild\Current\Bin\MSBuild.exe" .\vs2017\sudoku_ants.vcxproj /t:Build /p:Configuration=Release /p:Platform=x64
 if %errorlevel% equ 0 (
     echo.
     echo Build successful! Executable: .\vs2017\x64\Release\sudoku_ants.exe
